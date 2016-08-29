274c9aaeec4f9da7625ea2cc4823b6c664d241bc
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-08-29 00:19:30 -0300

    Adding possibility to have a steady person

--- doodle.love/person_model.lua
+++ doodle.love/person_model.lua
@@ -4,31 +4,32 @@ local player_model = { }
 function player_model.new(inlet)
     local self = { }
     self = player_model.parse(self, inlet)
-    
+
     -- ######################
     -- # UPDATING FUNCTIONS #
     -- ######################
     self.is_update_time = function(moment)
         local fact = false
-        
-        if moment - self.last_moment > self.rate then
+        local wait = moment - self.last_moment
+
+        if (wait > self.rate) and (self.rate > 0) then
             fact = true
             self.last_moment = moment
         end
-        
+
         return fact
     end
-    
+
     self.get_direction = function()
         local directions = { "left", "right", "up", "down" }
         return directions[love.math.random(1, 4)]
     end
-    
+
     self.walk = function(dx, dy)
         self.x = self.x + dx
         self.y = self.y + dy
     end
-    
+
     return self
 end
 
